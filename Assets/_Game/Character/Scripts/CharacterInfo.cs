2fde813509a449fa3f8bf5aae8b0c2d3af761816
Igor Kravchenko <68440873+LOK1fx@users.noreply.github.com>
2025-08-11 20:19:10 +0900

    speech synth base

---
+++ Assets/_Game/Character/Scripts/CharacterInfo.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace LOK1game
+{
+    public enum ESex : byte
+    {
+        None,
+        Male,
+        Female
+    }
+
+    public class CharacterInfo
+    {
+        
+    }
+}
---
+++ Assets/_Project/Editor/Windows/TTS/TTSGeneratorEditorWindow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class TTSGeneratorEditorWindow : MonoBehaviour
+{
+    
+}
